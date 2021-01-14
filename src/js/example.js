638a866cd087a51d99db7f45b2ee5264324ea9cc
Svyat <slava1511200@gmail.com>
2021-01-15 03:02:47 +0500

    initial

--- src/js/example.js
+++ src/js/example.js
@@ -1,6 +1,1 @@
-export const example = () => {
-  return (
-    'Sensible webpack 5 boilerplate using Babel and PostCSS ' +
-    'with a hot dev server and an optimized production build.'
-  )
-}
+export default () => console.log('alo kurwa')
